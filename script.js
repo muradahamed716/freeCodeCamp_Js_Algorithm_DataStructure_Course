e2cc229c32acf0d80484b7fe864af16d45c92c17
Murad <muradahamed6262@gmail.com>
2022-01-27 13:03:10 +0600

    Weired calculation practise

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let c=0.2;
 let d=0.1;
-console.log(a+d)//weired result
-
+console.log(c+d)//weired result
+console.log(c*d)//weired result
+console.log(c/d)
+console.log(c%d)
 const a=1.2;
 const b=0.4;
 console.log(a+b);
